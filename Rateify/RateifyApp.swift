91c0096ea482b594cbb5ec376bf4c0a5abe8b8e1
Antonio Odore <antohview@gmail.com>
2024-12-06 15:56:13 +0100

    Initial Commit

---
+++ Rateify/RateifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RateifyApp.swift
+//  Rateify
+//
+//  Created by Antonio Odore on 06/12/24.
+//
+
+import SwiftUI
+
+@main
+struct RateifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
